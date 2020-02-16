5f33a493c14d376835289574bd36381ed19b3338
Evgene Popolamov <popolamov908@gmail.com>
2020-02-16 20:08:40 +0300

    first commit

---
+++ Прототипное наследование/main.js
@@ -0,0 +1,89 @@
+// Работа с прототипами
+// важность: 5
+// В приведённом ниже коде создаются и изменяются два объекта.
+
+// Какие значения показываются в процессе выполнения кода?
+
+// let animal = {
+//   jumps: null
+// };
+// let rabbit = {
+//   __proto__: animal,
+//   jumps: true
+// };
+
+// alert( rabbit.jumps ); // ? (1)
+
+// delete rabbit.jumps;
+
+// alert( rabbit.jumps ); // ? (2)
+
+// delete animal.jumps;
+
+// alert( rabbit.jumps ); // ? (3)
+1 - true;
+2 - null;
+3 - undefined;
+
+// Алгоритм поиска
+// важность: 5
+// Задача состоит из двух частей.
+
+// У нас есть объекты:
+
+// let head = {
+//   glasses: 1
+// };
+
+// let table = {
+//   pen: 3
+// };
+
+// let bed = {
+//   sheet: 1,
+//   pillow: 2
+// };
+
+// let pockets = {
+//   money: 2000
+// };
+// С помощью свойства __proto__ задайте прототипы так, чтобы поиск любого свойства выполнялся по следующему пути: pockets → bed → table → head. Например, pockets.pen должно возвращать значение 3 (найденное в table), а bed.glasses – значение 1 (найденное в head).
+// Ответьте на вопрос: как быстрее получить значение glasses – через pockets.glasses или через head.glasses? При необходимости составьте цепочки поиска и сравните их.
+let head = {
+    glasses: 1
+};
+
+let table = {
+    pen: 3,
+    __proto__: head
+};
+
+let bed = {
+    sheet: 1,
+    pillow: 2,
+    __proto__: table
+};
+
+let pockets = {
+    money: 2000,
+    __proto__: bed
+};
+
+
+
+// Создайте новый объект с помощью уже существующего
+// важность: 5
+// Представьте, что у нас имеется некий объект obj, созданный функцией-конструктором – мы не знаем какой именно, но хотелось бы создать ещё один объект такого же типа.
+
+// Можем ли мы сделать так?
+
+// let obj2 = new obj.constructor();
+function Rabbit(name) {
+    this.name = name,
+        this.color = 'white',
+        this.age = 3
+};
+
+let obj = new Rabbit('peter');
+
+let obj2 = new obj.constructor('John');
