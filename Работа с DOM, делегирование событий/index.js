c4b6223a7f2060a61800e47d86dd01ef41483d65
Evgene Popolamov <popolamov908@gmail.com>
2020-05-05 21:52:04 +0300

    added event delegation

---
+++ Работа с DOM, делегирование событий/index.js
@@ -0,0 +1,22 @@
+// Получаем доступ к параграфам
+// let p = document.querySelectorAll("p");
+
+// С помощью цикла for вешаем событие на каждый параграф
+// for (let i = 0; i < p.length; i++) {
+//   p[i].addEventListener("click", function (event) {
+//     event.target.style.color = "blue";
+//   });
+// }
+// Но!!! если параграфов слишком много это затруднит работу нашего приложения
+
+document.getElementById("wrapper").addEventListener("click", function (event) {
+  let tagName = event.target.tagName.toLowerCase();
+
+  if (tagName === "p") {
+    event.target.style.color = "blue";
+  }
+
+  if (event.target.classList.contains("color")) {
+    event.target.style.color = "red";
+  }
+});
